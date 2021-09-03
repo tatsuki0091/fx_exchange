a7b7ed299ef64de5c2cbb490afdbfe9a5c3d6688
tatsuki0091 <rossosalvatore@icloud.com>
2021-09-03 10:17:17 -0700

    Complete the list function and the calculate function.

---
+++ pages/api/fx.ts
@@ -0,0 +1,13 @@
+
+
+export type Props = {
+    fxInfo: {
+        high: number,
+        open: number,
+        bid: number,
+        currencyPairCode: string,
+        ask: number,
+        low: number,
+    }[]
+}
+
---
+++ pages/types.ts
@@ -0,0 +1,3 @@
+export interface FX_INFO {
+    quotes:[]
+}
